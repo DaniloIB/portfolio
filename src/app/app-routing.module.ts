292034db4bd22a1e80a0398e4d9c68e3405290e6
DaniloIB <ibaniez.danilo@gmail.com>
2023-05-04 11:40:36 -0300

    feat: se crea el home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/pages/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent,
+    loadChildren: ()=>import('../app/modules/home/home.module').then(m=>m.HomeModule)
+    },
+  {
+    path: '**',
+    redirectTo: '/home',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
